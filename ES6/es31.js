f7aaf021d4defd50848ee0128e01767ef000185b
Johan Back <johan.backs@gmail.com>
2019-12-27 17:41:38 -0300

    chore: add 24/32 lesson

---
+++ ES6/es31.js
@@ -0,0 +1,13 @@
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+   
+  if(responseFromServer) {
+       resolve("We got the data")
+  } else {  
+    reject("Data not received")
+  }
+});
+makeServerRequest.then(result => {
+console.log(result)
+});
