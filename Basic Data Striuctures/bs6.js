7cec4608e98b671675e9c016ddee8d0f41f4d431
Johan Back <johan.backs@gmail.com>
2020-01-21 12:27:47 -0300

    chore: add lessons

---
+++ Basic Data Striuctures/bs6.js
@@ -0,0 +1,5 @@
+function forecast(arr) {
+  return arr.slice(2,4);
+}
+// do not change code below this line
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
---
+++ Basic Data Striuctures/bs7.js
@@ -0,0 +1,4 @@
+let thisArray = [true, true, undefined, false, null];
+let thatArray = [...thisArray];
+
+console.log(thatArray);
